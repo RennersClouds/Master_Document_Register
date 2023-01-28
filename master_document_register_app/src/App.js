00bc61aabe40b0fb28fa2a6becf3bd6817bce44e
Renner Renner <rennerclouds@gmail.com>
2023-01-28 17:47:14 +0100

    first commit top the MDR app

---
+++ master_document_register_app/src/App.js
@@ -0,0 +1,107 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>THIS IS THE MASTER DOCUENT REGISTER PAGE FOR ENGINEERING DELIVERABLES FOR PROJECTS</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
